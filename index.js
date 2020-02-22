330aa0cd2d1f395b8fb8efda57c1b583b013c14e
Admin <gc974517@ohio.edu>
2020-02-22 18:01:55 -0500

    Add API endpoints.

--- index.js
+++ index.js
@@ -25,22 +25,50 @@ client.on("message", (message) => {
             if (parameters.length > 1) {
                 switch(parameters[1].toLowerCase()){
                     case "tinybrain":
-                        console.log(typeof message.guild.id);
+                        message.channel.send("@Dj73960");
                         break;
                     case "link":
                         if (parameters.length === 3) {
                             link(message, parameters[2]);
                         } else {
-                            message.reply("Usage: !mc link <username>\nUse !mc for help.");
+                            message.reply("Usage: !mc link <username>\nUse !mc or !mc help for help.");
                         }
                         break;
                     case "unlink":
+                        if (parameters.length === 3) {
+                            unlink(message, parameters[2]);
+                        } else {
+                            message.reply("Usage: !mc unlink <username>");
+                        }
+                        break;
+                    case "create":
+                        if (parameters.length > 2) {
+                            create(message, parameters.slice(3).join(" "));
+                        } else {
+                            message.reply("Usage: !mc create <server name>\nUse !mc or !mc help for help.");
+                        }
+                        break;
+                    case "start":
+                        if (parameters.length > 2) {
+                            message.channel.send(parameters.slice(3).join(" "));
+                            start(message, parameters.slice(3).join(" "));
+                        } else {
+                            message.reply("Usage: !mc create <server name>\nUse !mc or !mc help for help.");
+                        }
+                        break;
+                    case "credits":
+                        if (parameters.length === 2) {
+                            credits(message, message.author.id);
+                        } else {
+                            message.reply("Usage: !mc create <server name>\nUse !mc or !mc help for help.");
+                        }
+                        break;
+                    case "help":
+                        help(message);
                         break;
                 }
             } else {
-                message.channel.send("!mc - Display this help page\n!mc start - Start your Minecraft server" +
-                "\n!mc link - Link your account to your Discord Id\n!mc create - Instantly boot up a vanilla minecraft server" +
-                "\n!mc credits - Display your remaining credits before you git fucked");
+                help(message);
             }
             break;
         default:
@@ -50,20 +78,100 @@ client.on("message", (message) => {
 function link(message, username){
     // Check if user input a username
     var options = {
-        uri: "https://www.google.com", // change to /user/link
+        uri: "https://35.222.79.226/user/link",
         XDiscordServer: message.guild.id,
         method: "POST",
         userId: message.author.id,
-        minecraftName: username
+        minecraftName: username,
+        json: true
     };
 
     rp(options)
-        .then(function () {
-            message.channel.send("Howdy World!");
+        .then(function (body) {
+            message.reply(message.content);
         })
         .catch(function (err) {
-            message.channel.send("Oh god oH fuck");
+            message.channel.send("Error using " + message.content);
         });
+}
+
+function unlink(message, username) {
+    // Check if user input a username
+    var options = {
+        uri: "https://35.222.79.226/user/unlink",
+        XDiscordServer: message.guild.id,
+        method: "POST",
+        userId: message.author.id,
+        minecraftName: username,
+        json: true
+    };
+
+    rp(options)
+        .then(function (body) {
+            message.reply(message.content);
+        })
+        .catch(function (err) {
+            message.channel.send("Error using " + message.content);
+        });
+}
+
+function create(message, serverName) {
+    var options = {
+        uri: "https://35.222.79.226/server/create",
+        XDiscordServer: message.guild.id,
+        method: "POST",
+        name: serverName,
+        creatorId: message.author.id,
+        json: true
+    };
+
+    rp(options)
+        .then(function (body) {
+            message.reply(message.content);
+        })
+        .catch(function (err) {
+            message.channel.send("Error using " + message.content);
+        });
+}
+
+function start(message, serverName) {
+    var options = {
+        uri: "https://35.222.79.226/server/start",
+        XDiscordServer: message.guild.id,
+        method: "POST",
+        name: serverName,
+        json: true;
+    };
+
+    rp(options)
+        .then(function (body) {
+            message.reply(message.content);
+        })
+        .catch(function (err) {
+            message.channel.send("Error using " + message.content);
+        });
+}
+
+function credits(message, userId) {
+    var options = {
+        uri: "https://35.222.79.226/user/credits?id=" + userId,
+        XDiscordServer: message.guild.id,
+        json: true
+    };
+
+    rp(options)
+        .then(function (body) {
+            message.reply(message.content);
+        })
+        .catch(function (err) {
+            message.channel.send("Error using " + message.content);
+        });
+}
+
+function help(message) {
+    message.channel.send("!mc - Display this help page\n!mc start - Start your Minecraft server" +
+                         "\n!mc link - Link your account to your Discord Id\n!mc create - Instantly boot up a vanilla minecraft server" +
+                         "\n!mc credits - Display your remaining credits before you git fucked");
 }
 
 client.login(auth.token);
